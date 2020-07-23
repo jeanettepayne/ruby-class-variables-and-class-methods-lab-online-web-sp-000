49e2cdabdc95cf00c3e74cc6d7b55dc9248e8453
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-23 22:10:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song 
   attr_accessor :name, :genre, :artist
   def initialize(name, genre, artist)
-    @name
-    @genre
-    @artist
+    @name = name
+    @genre = genre
+    @artist = artist
   end
 end
