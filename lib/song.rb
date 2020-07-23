e9d92a7315be65d3947acd8583aa5eb2b97cfc04
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-23 23:19:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
     @@genres.uniq
   end
   
-   def self.genre_count
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
@@ -38,13 +38,17 @@ class Song
     genre_count
   end
   
-#   def self.genre_count
-#     @@genres.inject(Hash.new(0)) do |hash, genre|
-#           hash[genre] += 1 
-#         else
-#           hash[genre] = 1
-#   end
-#   end
-#   genre_count
-# end
+def self.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1 
+    else
+      artist_count[artist] = 1 
+    end
+  end
+  artist_count
+end
+
+
 end
