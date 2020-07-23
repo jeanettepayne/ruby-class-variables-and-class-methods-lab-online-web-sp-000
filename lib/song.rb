e28142a14e255efa3c55d521178aba6a5e2adada
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-23 22:44:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,10 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      
+      if genre_count[genre]
+        genre_count += 1 
+      else
+        
     
   end
 end
