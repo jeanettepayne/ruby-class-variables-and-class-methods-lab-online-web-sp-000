bf5606a2fab178d5decfce80b851c9816c7e7712
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-23 22:21:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,13 @@ class Song
   
   @@count = 0
   
-  def self.count
-    @@count
-  end
-  
   def initialize(name, artist, genre)
     @@count =+ 1
     @name = name
     @genre = genre
     @artist = artist
   end
+  
+  
   
 end
